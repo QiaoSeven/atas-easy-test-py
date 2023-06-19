c8e7b1eb3b1ab1c16da9345cc148f864d76a462c
QiaoSeven <71541206+QiaoSeven@users.noreply.github.com>
2023-06-19 16:23:50 +0800

    Update py-selenium-run-one.sh
    Seven test change

--- shell/py-selenium-run-one.sh
+++ shell/py-selenium-run-one.sh
@@ -9,5 +9,5 @@ fi
 export ATAS_CONF=${ENV}
 pytest /root/atas-easy-test-py/testcase/auto-test/ --html=report.html
 #pytest /root/atas-easy-test-py/testcase/api/ --html=report.html
-echo 'This execution is completed!'
+echo 'Seven's execution is completed!'
 export ATAS_CONF=''
